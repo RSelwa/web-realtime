79545a40048331fd79bcaff26eff03880dd90745
SELWA Raphael <selwa.raphael@gmail.com>
2024-02-22 19:51:02 +0100

    ✨ create quizz

--- front/pages/my-quizz.tsx
+++ front/pages/my-quizz.tsx
@@ -2,15 +2,15 @@ import { DB_QUIZZ } from "@/constants"
 import { db } from "@/constants/db"
 import type { FirebaseDocumentWithId, Quizz } from "@/types"
 import { getItemFromLocalStorage } from "@/utils/storage"
-import { collection, getDocs, query, where } from "firebase/firestore"
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore"
 import Link from "next/link"
 import React, { useEffect, useState } from "react"
+import { useForm, type FieldValue, type FieldValues } from "react-hook-form"
 
-type Props = {}
-
-const MyQuizz = (props: Props) => {
+const MyQuizz = () => {
   const [quizzes, setquizzes] = useState<FirebaseDocumentWithId<Quizz>[]>([])
   const { id } = getItemFromLocalStorage("user", { id: "" })
+  const { handleSubmit, register, reset } = useForm<{ quizzName: string }>()
   const fetchQuizzes = async () => {
     try {
       const myQuizzes = await getDocs(
@@ -23,17 +23,40 @@ const MyQuizz = (props: Props) => {
       console.error(error)
     }
   }
+  const createQuizz = async ({ quizzName }: { quizzName: string }) => {
+    try {
+      const newQuizz: Quizz = {
+        ownerid: id,
+        questions: [],
+        title: quizzName
+      }
+      await addDoc(collection(db, DB_QUIZZ), newQuizz)
+      reset()
+      await fetchQuizzes()
+    } catch (error) {
+      console.error(error)
+    }
+  }
   useEffect(() => {
     fetchQuizzes()
   }, [])
 
   return (
     <div className="flex gap-2">
-      {quizzes.map((quizz, i) => (
-        <Link key={i} href={`/quizz/${quizz.id}`}>
-          {quizz.title}
-        </Link>
-      ))}
+      <form onSubmit={handleSubmit(createQuizz)}>
+        <fieldset>
+          <label htmlFor="quizzName">Quizz Name</label>
+          <input type="text" {...register("quizzName")} required />
+        </fieldset>
+        <button type="submit">Créer</button>
+      </form>
+      <div>
+        {quizzes.map((quizz, i) => (
+          <Link key={i} href={`/quizz/${quizz.id}`}>
+            {quizz.title}
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
